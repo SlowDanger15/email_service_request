f2e7d3a60d24f2de42cd1b262a179000e8d29380
SlowDanger15 <rohanprasad.2001@.gmail.com>
2025-04-06 00:25:14 -0400

    fixed key mismatch

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,20 +5,21 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Only POST requests allowed' });
   }
 
-  const { from_name, from_email, subject, message } = req.body;
+//   const { from_name, from_email, subject, message } = req.body;
+  const { name, email, subject, message } = req.body;
 
   try {
     const response = await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID,
-      process.env.EMAILJS_TEMPLATE_ID,
-      {
-        from_name,
-        from_email,
-        subject,
-        message,
-      },
-      process.env.EMAILJS_PUBLIC_KEY
-    );
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
+        {
+          from_name: name,
+          from_email: email,
+          subject,
+          message,
+        },
+        process.env.EMAILJS_PUBLIC_KEY
+      );
 
     return res.status(200).json({ success: true, response });
   } catch (error) {
